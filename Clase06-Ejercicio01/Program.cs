34379ac6a79de65f8a601cfe91a8ae02e5e4bfa7
William Parodi <jonwilliamparodid@gmail.com>
2022-09-13 21:18:16 -0300

    ejercicio Clase06Ejer01

---
+++ Clase06-Ejercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Clase06_Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNumeros = new int[20];
+            Random rnd = new Random();
+
+            for(int i = 0; i < arrayNumeros.Length; i++)
+            {
+                arrayNumeros[i] = rnd.Next(-100,100);
+                Console.WriteLine($"Numeros orignales : {arrayNumeros[i]}");
+            }
+
+
+            
+
+            
+            
+        }
+
+    
+    }
+}
